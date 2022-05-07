c2c5751398ceaeff648a0d42cef798abea6aa07c
Ömer Faruk GÖL <omerfarukgol@hotmail.com>
2022-05-07 22:29:24 +0300

    GradeAverage

--- src/GradeAverage/Main.java
+++ src/GradeAverage/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-    	// Merge at
+    	// Merge için
         // declare variables
         int mathematic, physice, chemical, turkish, history, music, total;
         String result;
