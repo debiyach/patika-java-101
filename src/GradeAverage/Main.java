ebfbecda1f9ba5882d837da4a1966952c0d8d32a
Ömer Faruk GÖL <omerfarukgol@hotmail.com>
2022-05-07 12:25:23 +0300

    Initialize commit

---
+++ src/GradeAverage/Main.java
@@ -0,0 +1,39 @@
+package GradeAverage;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // declare variables
+        int mathematic, physice, chemical, turkish, history, music, total;
+        String result;
+
+        // declare Scanner class
+        Scanner input = new Scanner(System.in);
+
+        // Get inputs
+
+        System.out.print("mathematic grade: ");
+        mathematic = input.nextInt();
+
+        System.out.print("physice grade: ");
+        physice = input.nextInt();
+
+        System.out.print("chemical grade: ");
+        chemical = input.nextInt();
+
+        System.out.print("turkish grade: ");
+        turkish = input.nextInt();
+
+        System.out.print("history grade: ");
+        history = input.nextInt();
+
+        System.out.print("music grade: ");
+        music = input.nextInt();
+
+        total = (mathematic + physice + chemical + turkish + history + music);
+        result = (total > 60) ? "Sınıfı Geçti" : "Sınıfta Kaldı";
+
+        System.out.print(result);
+    }
+}
